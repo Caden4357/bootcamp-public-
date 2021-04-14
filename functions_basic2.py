2ed131fdda77b1be65803fbc65759cd6e9fe8c3a
Caden <wilcox4357@gmail.com>
2021-04-13 17:45:23 -0700

    addedMoreFunctions

--- functions_basic2.py
+++ functions_basic2.py
@@ -60,5 +60,65 @@ def size_value(size, value):
     return new_lst
 print(size_value(5,10))
 
+#Create a function that takes a list and return that list with values reversed. Do this without creating a second list. (This challenge is known to appear during basic technical interviews.)
+#Example: reverse_list([37,2,1,-9]) should return [-9,1,2,37]
+def reverse_list(list):
+    list.reverse()
+    return list
+print(reverse_list([5,4,3,6,77,5,4,8]))
+#another way to reverse a list without functions 
+
+def reverse_list2(lst):
+    length = len(lst)-1
+    x = length
+    for i in range(len(lst)//2):
+        lst[i], lst[x-i] = lst[x-i], lst[i]
+    return lst
+print(reverse_list2([0,1,2,3]))
+
+#Calculate years to days
+
+def years_to_days(age):
+    year = 365
+    days = 0 
+    days = age * year
+    return days
+print(years_to_days(100))
+
+#Implement function sigma(num) that given a number, returns the sum of all positive integers up to number (inclusive). Ex.:sigma(3)=6(or1 + 2 + 3); sigma(5)=15(or1 + 2 + 3 + 4 + 5).
+
+def sigma(num):
+    new_list = []
+    total = 0
+    for i in range(1,num+1,1):
+        new_list.append(i)
+    for j in range(len(new_list)):
+        total = total + new_list[j]
+    return total    
+print(sigma(100))
+
+#sorting a list from smallest to greatest
+
+def sorting(lst):
+    for j in range(len(lst)-1):
+        for i in range(len(lst)-1-j):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+    return lst
+print(sorting([6,5,7,4,1,3,2]))
+
+#Create threesFives(n) that adds values from 1 and n (inclusive) if that value is not divisible by 3 or 5. Return the final sum.
+
+#For example, threesFives(10) returns 22 as it only returns the sum of 1, 2, 4, 7, and 8. On that example, it skips 3, 6, 9 as those are divisible by 3. It also skips 5, and 10 as it's divisible by 5.
+
+def threeFive(num):
+    sum = 0
+    new_list = []
+    for i in range(1,num+1,1):
+        if i % 3 != 0 and i % 5 !=0:
+            sum += i
+    return sum
+print(threeFive(10))
+
 
 
